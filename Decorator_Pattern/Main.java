e7a23769f86c7c531def217899a6ac7394f71b4e
Tanishq dhingra <dhingratanishq58@gmail.com>
2026-08-10 10:57:59 +0530

    Decorator Added

---
+++ Decorator_Pattern/Main.java
@@ -0,0 +1,60 @@
+package Decorator_Pattern;
+
+// Component
+interface Character {
+    String getAbilities();
+}
+
+// Concrete Component
+class Mario implements Character {
+    public String getAbilities() {
+        return "Mario";
+    }
+}
+
+// Decorator
+abstract class PowerUp implements Character {
+
+    protected Character character;
+
+    PowerUp(Character character) {
+        this.character = character;
+    }
+}
+
+// Concrete Decorators
+class Gun extends PowerUp {
+
+    Gun(Character character) {
+        super(character);
+    }
+
+    public String getAbilities() {
+        return character.getAbilities() + " + Gun";
+    }
+}
+
+class Star extends PowerUp {
+
+    Star(Character character) {
+        super(character);
+    }
+
+    public String getAbilities() {
+        return character.getAbilities() + " + Star";
+    }
+}
+
+// Main
+public class Main {
+
+    public static void main(String[] args) {
+
+        Character mario = new Mario();
+
+        mario = new Gun(mario);
+        mario = new Star(mario);
+
+        System.out.println(mario.getAbilities());
+    }
+}
